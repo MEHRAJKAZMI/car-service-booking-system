a32346a6fb6ad230be013e61d6c418319da96db0
MEHRAJ KAZMI <syedmehrajkazmi11@gmail.com>
2026-07-22 11:44:25 +0500

    Add MongoDB connection with Mongoose

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
+require('dotenv').config();
+
 const express = require('express');
+const connectDB = require('./config/db');
+
+connectDB();
 
 const app = express();
 
-require('dotenv').config();
 const PORT = process.env.PORT || 5000;
-
-
-
 
 app.get('/', (req, res) => {
   res.send('Car Service Booking System API is running v2');
 });
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
