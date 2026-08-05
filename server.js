5b36fe51845e092a1deafb6a473d7dd1da3cdc83
MEHRAJ KAZMI <syedmehrajkazmi11@gmail.com>
2026-08-05 11:37:27 +0500

    Restore server.js and remove Vehicle/Service modules per updated requirements

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ const roleRoutes = require('./routes/roleRoutes');
 const permissionRoutes = require('./routes/permissionRoutes');
 const userRoutes = require('./routes/userRoutes');
 const shopRoutes = require('./routes/shopRoutes');
-const vehicleRoutes = require('./routes/vehicleRoutes');
-const serviceRoutes = require('./routes/serviceRoutes');
 const connectDB = require('./config/db');
 const errorHandler = require('./middlewares/errorHandler');
 
@@ -28,8 +26,6 @@ app.use('/api/roles', roleRoutes);
 app.use('/api/permissions', permissionRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/shops', shopRoutes);
-app.use('/api/vehicles', vehicleRoutes);
-app.use('/api/services', serviceRoutes);
 
 app.use((req, res, next) => {
   res.status(404).json({ success: false, message: `Route not found: ${req.originalUrl}` });
